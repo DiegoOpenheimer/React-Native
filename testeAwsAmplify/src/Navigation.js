32b03c159c94a9724d5671913517a569618ca173
Diego Alves Openheimer <diego.alves@Administradors-MacBook-Pro.local>
2020-01-27 14:34:13 -0300

    Adicionado requisição para trazer a lista de posts.

--- testeAwsAmplify/src/Navigation.js
+++ testeAwsAmplify/src/Navigation.js
@@ -3,13 +3,14 @@ import {createStackNavigator} from 'react-navigation-stack';
 
 import Login from './login/index';
 import Account from './createAccount/index';
-import Item from './item/index'
+import Item from './item/index';
 import Home from './home/index';
-import Post from './post/post'
+import Post from './post/post';
+import Posts from './posts/posts';
 
 const InitialNavigator = createStackNavigator({Login, Account});
 
-const HomeNavigator = createStackNavigator({Home, Item, Post});
+const HomeNavigator = createStackNavigator({Home, Item, Post, Posts});
 
 const SwitchNavigator = createSwitchNavigator({
   InitialNavigator,
